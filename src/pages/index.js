b112d91d1dbcabac26bde5da9a656f824cef133b
Meraki <ilham.anasruloh@student.uny.ac.id>
2021-11-01 21:35:02 +0700

    button with icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -130,13 +130,17 @@ const IndexPage = () => (
               See Projects
             </a>
           </div>
+
+
+          {/* Button with Icon
           <button
             type="button"
-            className="hidden inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             <CheckIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
             Publish
           </button>
+          */}
         </div>
       </div>
     </section>
